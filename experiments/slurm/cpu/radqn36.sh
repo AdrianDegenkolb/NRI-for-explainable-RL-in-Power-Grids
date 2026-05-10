ff2aacb160738683a74515e14797918f9b12eee0
Adrian Degenkolb <github@adrian-degenkolb.de>
2026-05-10 23:08:44 +0200

    Increase training duration for dqn and sac

--- experiments/slurm/cpu/radqn36.sh
+++ experiments/slurm/cpu/radqn36.sh
@@ -16,7 +16,7 @@ sbatch << EOF
 #SBATCH --error=results/experiments/${experiment_name}/out/error_radqn_s${seed}.%j.log
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=64
-#SBATCH --time=20:00:00
+#SBATCH --time=72:00:00
 #SBATCH --mem=200G
 #SBATCH --partition=cpu_il,cpu
 
@@ -36,6 +36,7 @@ PYTHONPATH=\$(pwd)/src python experiments/train.py \
     experiment=long \
     experiment.seed=${seed} \
     experiment.name=${experiment_name}_s${seed} \
+    experiment.post_training_evaluation.env_name=l2rpn_wcci_2020_test \
     training.train_batch_size=16 \
     env=case36 \
     model.encoder.max_degree=20 \
